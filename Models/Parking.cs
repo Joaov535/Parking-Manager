700b87a074061fad36162a6d5c7d26db87941c51
Joaov535 <joaovictorcruz535@gmail.com>
2024-09-02 22:59:23 -0300

    fix: atribuição de ID

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -6,11 +6,12 @@ namespace ParkingManager.Models;
 
 public class Parking : CustomModel
 {
-    public Guid Id;
+    public Guid Id { get; init; }
     public required string Name;
 
     public Parking(AppDbContext context) : base(context)
     {
+        Id = Guid.NewGuid();
     }
 
     public required ICollection<Spot> Spots { get; set; }
